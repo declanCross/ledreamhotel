de2783e42a701d416ab06e30e8b317068ccd62ce
declanCross <declan_cross@outlook.com>
2023-10-12 11:22:52 +1100

    big repair and add sub-feature pages

---
+++ resources/scripts/mNav.js
@@ -0,0 +1,21 @@
+//Load or remove dropdown content when clicking the three lines on the navbar.
+document.getElementById("m-nav-more").addEventListener("click", (event) => {
+	event.preventDefault();
+	let dropdownContent = document.getElementById("dropdown-content");
+	let mNav = document.getElementById("m-nav");
+	if (dropdownContent.style.display === "flex") {
+		dropdownContent.style.display = "none";
+		mNav.style.backgroundColor = "rgba(251, 252, 254, 0.8)";
+	} else {
+		dropdownContent.style.display = "flex";
+		mNav.style.backgroundColor = "rgba(251, 252, 254, 1)";
+	}
+});
+//Remove dropdown content when clicking the x within dropdown content.
+document.getElementById("dropdown-exit").addEventListener("click", (event) => {
+	event.preventDefault();
+	let dropdownContent = document.getElementById("dropdown-content");
+	let mNav = document.getElementById("m-nav");
+	dropdownContent.style.display = "none";
+	mNav.style.backgroundColor = "rgba(251, 252, 254, 0.8)";
+});
