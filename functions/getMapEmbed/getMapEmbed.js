aef029fe721fda996501303827a4a5bdb444ca0e
declanCross <declan_cross@outlook.com>
2023-12-09 11:09:58 +1100

    improve accessibility of google maps api

--- functions/getMapEmbed/getMapEmbed.js
+++ functions/getMapEmbed/getMapEmbed.js
@@ -1,7 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 exports.handler = async function (event, context) {
 	try {
-		const iframeHtml = `<iframe allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API_KEY}&q=Le+Dream+Hotel,Tamag,Vigan+City,Ilocos+Sur,Philippines"></iframe>`;
+		const iframeHtml = `<iframe allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API_KEY}&q=Le+Dream+Hotel,Tamag,Vigan+City,Ilocos+Sur,Philippines" title="Google map of Le Dream Hotel location"></iframe>`;
 		return { statusCode: 200, body: iframeHtml };
 	} catch (error) {
 		return { statusCode: 500, body: error.toString() };
